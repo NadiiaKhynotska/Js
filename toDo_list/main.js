0054147688fa637ab2ca0a45203d1ab655ac849a
NadiiaKhynotska <nadinyman@gmail.com>
2023-06-14 00:37:20 +0300

    simple todo list done

--- toDo_list/main.js
+++ toDo_list/main.js
@@ -2,6 +2,30 @@ const button = document.querySelector('.input-container > button');
 const input = document.querySelector('.input-container > input');
 const list = document.querySelector('.todo-list');
 
+let store = JSON.parse(localStorage.getItem('todo-task')) || [];
+// if (store.length !== 0) {
+//     for (const storeElement of store) {
+//         const li = document.createElement('li');
+//         const deleteBtn = document.createElement('button');
+//         deleteBtn.classList.add('delete-btn');
+//         li.classList.add('todo-task');
+//
+//         li.innerText = storeElement;
+//         deleteBtn.innerText = 'Delete';
+//
+//         li.appendChild(deleteBtn);
+//         list.appendChild(li);
+//
+//         deleteBtn.addEventListener('click', key => {
+//             list.removeChild(li);
+//             let start = store.indexOf(storeElement)
+//             console.log(start)
+//             store.splice(start,1)
+//             console.log(store)
+//             localStorage.setItem('todo-task', JSON.stringify(store))
+//         })
+//     }
+// }
 button.addEventListener('click', () => {
     const li = document.createElement('li');
     const deleteBtn = document.createElement('button');
@@ -12,6 +36,9 @@ button.addEventListener('click', () => {
 
     if (li.innerText.length > 0) {
         deleteBtn.innerText = 'Delete';
+        store.push(input.value);
+        localStorage.setItem('todo-task', JSON.stringify(store))
+
         li.appendChild(deleteBtn);
         list.appendChild(li);
         input.value = '';
@@ -21,7 +48,43 @@ button.addEventListener('click', () => {
     }
 
 
-    deleteBtn.addEventListener('click', () => {
-        list.removeChild(li);
-    })
+    // deleteBtn.addEventListener('click', key => {
+    //         list.removeChild(li);
+    //
+    // })
+    store = JSON.parse(localStorage.getItem('todo-task'))
+   for (const storeElement of store) {
+
+        deleteBtn.addEventListener('click', key => {
+            list.removeChild(li);
+            let start = store.indexOf(storeElement)
+
+            store.splice(start,1)
+            localStorage.setItem('todo-task', JSON.stringify(store))
+        })
+
+    }
+
 });
+
+for (const storeElement of store) {
+
+    const li = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    li.classList.add('todo-task');
+    deleteBtn.classList.add('delete-btn');
+    li.innerText = storeElement;
+    deleteBtn.innerText = 'Delete';
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+    deleteBtn.addEventListener('click', key => {
+            list.removeChild(li);
+            let start = store.indexOf(storeElement)
+
+            store.splice(start,1)
+
+            localStorage.setItem('todo-task', JSON.stringify(store))
+        })
+
+}
